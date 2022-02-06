a0761b12bb64b9d4158c0a4ad2fe325673eb6f28
zabih <mdzabihhaqqani@gmail.com>
2022-02-06 18:50:18 +0530

    feat : added tailwind colors

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["#ef4444","##f59e0b","#eab308","#84cc16","#22c55e","#10b981","#14b8a6","#06b6d4","#0ea5e9","#3b82f6","#4f46e50","#a855f7","#f43f5e"]
+
+// selecting button
+let btn = document.getElementById("btn");
+
+//selecting background
+let color = document.querySelector(".color")
+
+
+btn.addEventListener("click", function clickHandler(){
+
+    let randomNumber = random()
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+
+});
+
+//to get a random number everytime button is clicked
+function random(){
+    return Math.floor(Math.random()*colors.length)
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,24 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const colors = ["#475569", "#4b5563"," #52525b"]
+
+
+
+
+// selecting button
+let btn = document.getElementById("btn");
+
+//selecting background
+let color = document.querySelector(".color")
+
+
+btn.addEventListener("click", function clickHandler(){
+
+    let randomNumber = random()
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+
+});
+
+//to get a random number everytime button is clicked
+function random(){
+    return Math.floor(Math.random()*colors.length)
+}

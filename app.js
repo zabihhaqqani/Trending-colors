b356834701984f6392091c76b4a93d3df8f980a9
zabih <mdzabihhaqqani@gmail.com>
2022-02-06 19:59:50 +0530

    style : removed unwanted #

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["#ef4444","##f59e0b","#eab308","#84cc16","#22c55e","#10b981","#14b8a6","#06b6d4","#0ea5e9","#3b82f6","#4f46e50","#a855f7","#f43f5e"]
+const colors = ["#ef4444","#f59e0b","#eab308","#84cc16","#22c55e","#10b981","#14b8a6","#06b6d4","#0ea5e9","#3b82f6","#4f46e50","#a855f7","#f43f5e"]
 
 // selecting button
 let btn = document.getElementById("btn");
